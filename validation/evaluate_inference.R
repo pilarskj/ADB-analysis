7fc7c4dfb048dafc027c370170d6a3f798f4f648
pilarskj <julia.pilarski@gmail.com>
2025-06-02 11:55:50 +0200

    add validation

---
+++ validation/evaluate_inference.R
@@ -0,0 +1,109 @@
+## ---------------------------
+##
+## Script for evaluating phylodynamic inference under the Age-Dependent Branching model (validation/ well-calibrated simulation study)
+##
+## Author: Julia Pilarski
+## Email: julia.pilarski@bsse.ethz.ch
+##
+## Date Created: 2025-06-02
+##
+## ---------------------------
+
+setwd("~/Projects/P1_AgeDependentTrees/ADB-analysis/validation")
+inf_dir = "~/Projects/P1_AgeDependentTrees/validation/calibration/inference" ## to change
+
+library(tracerer)
+library(HDInterval)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(tibble)
+library(ggplot2)
+theme_set(theme_classic(base_size = 14))
+
+## ---------------------------
+
+# Function for calculating the median and 95% HPD interval for all inferred parameters from a log table
+get_sumstats_from_log <- function(log, parameters){
+  stats = sapply(parameters, function(x) {
+    # get posterior estimates
+    ix = match(x, colnames(log))
+    posterior = log[, ix]
+    # calculate median and HPD interval  
+    hpd = hdi(posterior, credMass = 0.95)
+    stats = c('lower' = hpd[[1]], 'median' = median(posterior), 'upper' = hpd[[2]])
+  }) %>% 
+    t() %>% as.data.frame() %>% rownames_to_column('parameter')
+  return(stats)
+}
+
+# Function for processing a log file
+get_estimates <- function(log_fs, parameters) {
+  
+  # get parameter estimates
+  data = lapply(log_fs, function(f) {
+    
+    # parse beast log 
+    log = remove_burn_ins(parse_beast_tracelog_file(f), burn_in_fraction = 0.1)
+    
+    # get summary statistics
+    stats = get_sumstats_from_log(log, parameters)
+    
+    # add ID and chain length
+    stats$ID = str_extract(sapply(str_split(f, '/'), tail, 1), "[0-9]+")
+    stats$chainLength = log$Sample[nrow(log)]
+    
+    # get ESS per parameter
+    ess = calc_esses(log, sample_interval = 1000)
+    stats$ESS = as.numeric(ess[match(stats$parameter, names(ess))])
+    
+    return(stats)
+  }) %>% bind_rows()
+  
+  return(data)
+}
+
+## ---------------------------
+
+## load ground truth parameters
+truth = read.csv("tree_data.csv")
+
+## load inferred parameters
+log_fs = paste0(inf_dir, '/inference_', c(1:100), '.log')
+parameters = c('shape', 'lifetime', 'deathprob')
+data = get_estimates(log_fs, parameters)
+
+## check convergence
+all(data$ESS >= 200, na.rm = T) # all chains have sufficient ESS
+data %>% filter(is.na(ESS)) # all have shape = 1
+truth[data %>% filter(is.na(ESS)) %>% pull(ID) %>% as.numeric(), ] # indeed, the real shape is 1
+
+## align estimates &calculate coverage
+df = data %>% 
+  left_join(truth %>%
+              pivot_longer(cols = parameters, names_to = "parameter", values_to = "true") %>%
+              select(ID = tree, parameter, true) %>%
+              mutate(ID = as.character(ID)), by = c("ID", "parameter")) %>%
+  mutate(correct = ifelse(true >= lower & true <= upper, T, F),
+         parameter = factor(parameter, levels = parameters)) 
+cov = df %>% group_by(parameter) %>% summarize(cov = round(sum(correct) * 100 / n(), 1)) 
+
+## plot results
+ggplot(df, aes(x = true, y = median, color = correct)) +
+  geom_point(size = 0.7, show.legend = F) + 
+  geom_errorbar(aes(ymin = lower, ymax = upper), alpha = 0.5, show.legend = F) +
+  geom_text(data = cov, mapping = aes(x = -Inf, y = -Inf, label = paste(cov, "%")), hjust = -6.5, vjust = -1, inherit.aes = F) + 
+  geom_line(aes(x = true, y = true), inherit.aes = F, alpha = 0.25) + 
+  scale_color_manual(values = c("FALSE" = "#F8766D", "TRUE" = "#009E73")) + 
+  labs(x = "True value", y = "Posterior median\nwith 95% HPD interval") +
+  facet_wrap(~parameter, scales = "free", labeller = as_labeller(
+    c("shape" = "'Shape'~italic(k)", "lifetime" = "'Mean lifetime'~italic(l)", "deathprob" = "'Death probability'~italic(d)"), 
+    label_parsed))
+ggsave("calibration.pdf", width = 10, height = 4)
+
+
+
+
+
+
+
---
+++ validation/simulate_trees.R
@@ -0,0 +1,53 @@
+## ---------------------------
+##
+## Script for simulating phylogenetic trees under the Age-Dependent Branching model for validation
+##
+## Author: Julia Pilarski
+## Email: julia.pilarski@bsse.ethz.ch
+##
+## Date Created: 2025-05-30
+##
+## ---------------------------
+
+setwd("~/Projects/P1_AgeDependentTrees/ADB-analysis/validation")
+
+## install packages
+library(devtools)
+install("~/Projects/scTreeSim") ## tree simulator cloned from https://github.com/sccevo/scTreeSim.git
+library(scTreeSim)
+library(ape)
+
+## settings
+set.seed(2)
+nsim = 100 # number of trees
+
+## draw model parameters
+shape = round(rlnorm(n = nsim, meanlog = 2, sdlog = 1)) 
+lifetime = rlnorm(n = nsim, meanlog = 2, sdlog = 1)
+deathprob = rexp(n = nsim, rate = 10)
+rho = rbeta(n = nsim, shape1 = 2, shape = 5)
+scale = lifetime / shape
+ntips = 100
+
+## simulate trees
+trees = lapply(c(1:nsim), function(i) {
+  repeat { 
+    tree = sim_adb_ntaxa_samp(ntaxa = ntips, a = scale[i], b = shape[i], d = deathprob[i], rho = rho[i])
+    if (!is.null(tree)) break
+  }
+  tree = tree@phylo
+  tree$tip.label = c(1:Ntip(tree))
+  return(tree) }) 
+lapply(c(1:nsim), function(i) write.tree(trees[[i]], paste0('trees/tree_', i, '.newick')))
+
+## store ground-truth information
+tree_data = data.frame(
+  tree = c(1:nsim), 
+  shape = shape,
+  lifetime = lifetime,
+  deathprob = deathprob,
+  rho = rho,
+  origin = sapply(trees, function(t) t$origin),
+  treeHeight = sapply(trees, function(t) max(node.depth.edgelength(t))),
+  treeLength = sapply(trees, function(t) sum(t$edge.length)))
+write.csv(tree_data, file = 'tree_data.csv', quote = F, row.names = F)
